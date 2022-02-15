69e7d1fd03af752134754ac361abf893516010c8
Ting Zhe Yan <1884616+ytz@users.noreply.github.com>
2022-02-14 22:22:11 -0800

    Init dash app

---
+++ dash-app.py
@@ -0,0 +1,38 @@
+from dash import Dash, html, dcc, Input, Output
+import altair as alt
+from vega_datasets import data
+
+
+# Read in global data
+df = data.barley()
+
+# Setup app and layout/frontend
+app = Dash(
+    __name__, external_stylesheets=["https://codepen.io/chriddyp/pen/bWLwgP.css"]
+)
+app.layout = html.Div(
+    [
+        html.Iframe(
+            id="boxplot",
+            style={"border-width": "0", "width": "100%", "height": "300px"},
+        ),
+        dcc.Dropdown(
+            id="widget-site",
+            value="Morris",  # REQUIRED to show the plot on the first page load
+            options=[{"label": col, "value": col} for col in df.site.unique()],
+        ),
+    ]
+)
+
+# Set up callbacks/backend
+@app.callback(Output("boxplot", "srcDoc"), Input("widget-site", "value"))
+def plot_altair(site):
+    df_plot = df.query(f"site == '{site}'")
+    chart = (
+        alt.Chart(df_plot).mark_boxplot().encode(y="variety", x="yield").interactive()
+    )
+    return chart.to_html()
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
